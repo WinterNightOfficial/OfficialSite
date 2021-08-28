79103fd08e3fc924091d96f34de543cbf60ae444
bcoffee <blackcat980630@gmail.com>
2021-08-28 09:31:27 +0800

    Update pop method

--- docs/RTCat-Count-Down/pop-rtc.js
+++ docs/RTCat-Count-Down/pop-rtc.js
@@ -1,3 +1,11 @@
+window.onload = function(){
+    var imgBox = document.getElementById("imgBoxBtn");
+    if(imgBox){
+        imgBox.addEventListener('pointerdown', PopRTCDown, false);
+        imgBox.addEventListener('pointerup', PopRTCUp, false);
+    }
+}
+
 function PopRTCDown(){
     document.getElementById("imgBox").src = "images/icon-large.png";
 }
