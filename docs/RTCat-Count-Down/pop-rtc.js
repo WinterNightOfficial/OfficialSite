0fe65bb37237a949aa01f9d35892d147b810bb68
bcoffee <blackcat980630@gmail.com>
2021-08-28 17:41:27 +0800

    Adjust sound effect

--- docs/RTCat-Count-Down/pop-rtc.js
+++ docs/RTCat-Count-Down/pop-rtc.js
@@ -1,16 +1,22 @@
 window.onload = function(){
     var imgBox = document.getElementById("imgBoxBtn");
+
     if(imgBox){
         imgBox.addEventListener('pointerdown', PopRTCDown, false);
         imgBox.addEventListener('pointerup', PopRTCUp, false);
     }
-}
 
-function PopRTCDown(){
-    var audio = document.getElementById("pop");
-    audio.play();
-    document.getElementById("imgBox").src = "images/icon-large.png";
-}
-function PopRTCUp(){
-    document.getElementById("imgBox").src = "images/icon.jpg";
+    function PopRTCDown(){
+        document.getElementById("imgBox").src = "images/icon-large.png";
+        sound();
+    }
+
+    function PopRTCUp(){
+        document.getElementById("imgBox").src = "images/icon.jpg";
+    }
+
+    function sound(){
+        var pop = new Audio("pop.mp3");
+        pop.play();
+    }
 }
