0864d45d88f9ff4469de60764024991b61f2619f
bcoffee <blackcat980630@gmail.com>
2021-08-28 08:52:15 +0800

    Update countdown.js

--- docs/RTCat-Count-Down/countdown.js
+++ docs/RTCat-Count-Down/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("December 3, 2021 00:00:00").getTime();
+var countDownDate = new Date("February 3, 2021 00:00:00").getTime();
 
 var x = setInterval(function(){
     var now = new Date().getTime();
