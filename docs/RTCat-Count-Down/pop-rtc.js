97fc2d04dafe2632f4b2fff1af7f5f8b66a32090
bcoffee <blackcat980630@gmail.com>
2021-08-28 18:31:50 +0800

    Pop count update

--- docs/RTCat-Count-Down/pop-rtc.js
+++ docs/RTCat-Count-Down/pop-rtc.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     var imgBox = document.getElementById("imgBoxBtn");
+    var count = 0;
 
     if(imgBox){
         imgBox.addEventListener('pointerdown', PopRTCDown, false);
@@ -9,6 +10,7 @@ window.onload = function(){
     function PopRTCDown(){
         document.getElementById("imgBox").src = "images/icon-large.png";
         sound();
+        PopCount();
     }
 
     function PopRTCUp(){
@@ -18,5 +20,10 @@ window.onload = function(){
     function sound(){
         var pop = new Audio("pop.mp3");
         pop.play();
+    }
+
+    function PopCount(){
+        count++;
+        document.getElementById("count").innerHTML = count;
     }
 }
