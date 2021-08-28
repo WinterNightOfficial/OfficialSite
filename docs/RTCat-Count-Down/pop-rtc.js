ed4adf4f31e422a5ade3f9abb8e83eb54fae74c2
bcoffee <blackcat980630@gmail.com>
2021-08-28 09:11:00 +0800

    Add pop function

---
+++ docs/RTCat-Count-Down/pop-rtc.js
@@ -0,0 +1,6 @@
+function PopRTCDown(){
+    document.getElementById("imgBox").src = "images/scarecat.jpg";
+}
+function PopRTCUp(){
+    document.getElementById("imgBox").src = "images/icon.jpg";
+}
